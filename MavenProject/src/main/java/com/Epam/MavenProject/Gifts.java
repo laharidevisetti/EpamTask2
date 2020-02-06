356039f768aebfe905794b6a53c0c9c1302c57a3
laharidevisetti <39005714+laharidevisetti@users.noreply.github.com>
2020-02-06 22:41:20 +0530

    Maven project which contains programs(in the scenario of given Task)

---
+++ MavenProject/src/main/java/com/Epam/MavenProject/Gifts.java
@@ -0,0 +1,15 @@
+package com.Epam.MavenProject;
+import java.util.ArrayList;
+public class Gifts {
+	public int weight;
+	public int cost;
+	public static ArrayList<Integer> wgt = new ArrayList<Integer>();
+	public int getWeight(){
+		int totalWeight = 0;
+		for(int i = 0;i < wgt.size(); i++){
+			totalWeight += wgt.get(i);
+		}
+		return totalWeight;
+	}
+	
+}
---
+++ MavenProject/src/main/java/com/Epam/MavenProject/Sweets.java
@@ -0,0 +1,10 @@
+package com.Epam.MavenProject;
+
+public class Sweets extends Gifts {
+	public Sweets(int w,int p){
+		weight = w;
+		cost = p;
+		wgt.add(w);
+	}
+
+}
